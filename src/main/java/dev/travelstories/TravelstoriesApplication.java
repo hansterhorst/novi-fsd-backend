1092c0b8473a3b6cd297245d6925f7be5534c90a
hansterhorst <hans.novi@icloud.com>
2022-01-04 14:43:43 +0100

    project setup

---
+++ src/main/java/dev/travelstories/TravelstoriesApplication.java
@@ -0,0 +1,13 @@
+package dev.travelstories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelstoriesApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(TravelstoriesApplication.class, args);
+   }
+
+}
---
+++ src/test/java/dev/travelstories/TravelstoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.travelstories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelstoriesApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
