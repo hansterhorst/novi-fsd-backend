eae5bd90490eb1e799e7f857fc514cd1209b95a7
hansterhorst <hans.novi@icloud.com>
2022-01-16 13:01:42 +0100

    add findByEmailOrUsername

--- src/main/java/dev/travelstories/repositories/UserRepository.java
+++ src/main/java/dev/travelstories/repositories/UserRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
    Optional<User> findByEmail(String email);
-
+   Optional<User> findByEmailOrUsername(String email, String username);
+   Boolean existsByUsername(String username);
    Boolean existsByEmail(String email);
 }
--- src/main/java/dev/travelstories/security/CustomUserDetailsService.java
+++ src/main/java/dev/travelstories/security/CustomUserDetailsService.java
@@ -31,7 +31,7 @@ public class CustomUserDetailsService implements UserDetailsService {
    @Override
    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
 
-      User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() ->
+      User user = userRepository.findByEmailOrUsername(usernameOrEmail, usernameOrEmail).orElseThrow(() ->
          new UsernameNotFoundException("User not found with username or email." + usernameOrEmail));
 
       return new org.springframework.security.core.userdetails.User(user.getEmail(),
